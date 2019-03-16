7fd53a34afcaa6dfcb5b9a37deb2234648344d12
baylo014 <baylo014@umn.edu>
2019-03-16 17:43:52 -0500

    TODO

--- password.py
+++ password.py
@@ -6,6 +6,8 @@ def main():
     sha = hashlib.sha1(bytes(" ".join(sys.argv[1:]), 'utf-8')).hexdigest()
     suffix = sha[0:5]
     r = requests.get(url = "https://api.pwnedpasswords.com/range/"+suffix)
+    #TODO: Figure out how to remove the last char from the 2nd array element for each return
+    # Essentially do what is on line 14:47-64 on line 11:34-46
     dataList = list(map(lambda x:x.split(":"),r.text.split("\n")))
     for item in dataList:
         if (sha[5:].upper() in item):
